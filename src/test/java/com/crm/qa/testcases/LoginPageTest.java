851f0d731adc7a1a7cafdc983d80ef60fbc1866e
Nagesh Phaniraj <nageshphaniraj2017@gmail.com>
2019-05-04 19:53:53 +0530

    added log4j

--- src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -11,6 +11,17 @@ import com.crm.qa.pages.LoginPage;
 import com.crm.qa.util.TestUtil;
 
 public class LoginPageTest extends TestBase {
+	//What is log? : capturing info/activities at the time of program execution. 
+	// types of logs:
+		//1. info
+		//2. warn
+		//3. debug
+		//4. fatal
+		
+	//how to generate the logs? : use Apache log4j API (log4j jar)
+	//How it works? : it reads log 4j configuration from log4j.properties file
+	//where to create: create inside resources folder
+	
 	LoginPage loginPage;
 	HomePage homepage;
 
